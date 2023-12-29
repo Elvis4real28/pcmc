64700d80b50245a2e79df70e7e2ba1915c2a5abf
Elvis4real28 <113725078+Elvis4real28@users.noreply.github.com>
2023-12-29 23:01:22 +0000

    Add files via upload

---
+++ pcmc/donate.php
@@ -0,0 +1,7 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Process donation form submission
+    $amount = $_POST["amount"];
+    // Add your logic to handle the donation (e.g., save to a database)
+}
+?>
