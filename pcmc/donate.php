86ae18f2ce56d9761b5f669f893a9973cccd0e98
Elvis4real28 <113725078+Elvis4real28@users.noreply.github.com>
2023-12-29 23:44:50 +0000

    Delete pcmc directory

--- pcmc/donate.php
+++
@@ -1,7 +0,0 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Process donation form submission
-    $amount = $_POST["amount"];
-    // Add your logic to handle the donation (e.g., save to a database)
-}
-?>
--- pcmc/volunteer.php
+++
@@ -1,8 +0,0 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Process volunteer sign-up form submission
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    // Add your logic to handle volunteer sign-up (e.g., save to a database)
-}
-?>
